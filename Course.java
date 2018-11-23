495a2dced951a27f1f60bf70dc0027fff1507404
LuoJinRui <34363750+JinRLuo@users.noreply.github.com>
2018-11-24 01:41:07 +0800

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,12 @@
+package com.zhbit;
+
+public class Course {
+
+	String CourseNum;
+	String CourseName;
+
+	Course(String CourseNum, String CourseName) {
+		this.CourseNum = CourseNum;
+		this.CourseName = CourseName;
+	}
+}
