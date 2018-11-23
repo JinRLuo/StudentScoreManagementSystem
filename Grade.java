db478e61563e024662cf079132c4e65e9bb65700
LuoJinRui <34363750+JinRLuo@users.noreply.github.com>
2018-11-24 01:41:47 +0800

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,14 @@
+package com.zhbit;
+
+public class Grade {
+
+	String num;
+	String CourseNum;
+	int Grades;
+	
+	Grade(String num,String CourseNum,int Grades){
+		this.num = num;
+		this.CourseNum = CourseNum;
+		this.Grades = Grades;
+	}
+}
